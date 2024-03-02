d7f5e3e331fde1cd5057a4fb63761a3ca18ef363
Himalaya <himalaya341@gmail.com>
2024-03-03 00:36:13 +0530

    second commit - finish project

---
+++ Dynamic-interst-calculator/src/components/Results.jsx
@@ -0,0 +1,40 @@
+import { calculateInvestmentResults, formatter } from "../util/investment";
+
+export function Results({ input }) {
+  const results = calculateInvestmentResults(input);
+  const initalInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initalInvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
